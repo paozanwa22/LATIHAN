99550379191a36e8581c0a5daf5f0a329b616ad8
paozanwa22 <fauzanwadi7@gmail.com>
2020-09-02 12:02:17 +0800

    menambah pemahaman tentang functions

--- JS/function.js
+++ JS/function.js
@@ -18,11 +18,47 @@
 // alert( jumlahDuaBilangan( 1,2 ) );
 
 
-var jumlahVolumeKubus = function( a, b ){
-    var total;
-    total = (a*a*a) + (b*b*b);
+// var jumlahVolumeKubus = function( a, b ){
+//     var total;
+//     total = (a*a*a) + (b*b*b);
 
-    return total;
-}
+//     return total;
+// }
 
-alert('Hasil Menjumlahkan Volume 2 Kubus: ' + jumlahVolumeKubus( 8, 3 ) );
+// alert('Hasil Menjumlahkan Volume 2 Kubus: ' + jumlahVolumeKubus( 8, 3 ) );
+
+// -----    ---
+// functon tambah(a,b) {
+//     return a+b;
+// }
+
+// function kali(a,b) {
+//     return a*b;
+// }
+
+// var hasil = kali(tambah(1,2), tambah(4,5));
+// console.log(hasil);
+
+// -----    ----
+// function tambah() {
+//     return arguments;
+// }
+// var coba = tambah(5,10,20,"hi",false);
+// console.log(coba);
+    // arguments adalah sebuat variable yang berbentuk Arry yang bisa kita looping isinya.
+    // Argumen yang di abaikan oleh functon tapi tetap tersimpan di arguments di dalam function tambah
+
+// ----- ---- ---
+// function tambah() {
+//     var hasil = 0;
+//     for( var i = 0; i < arguments.length; i++ ) {
+//         hasil += arguments[i];
+//     }
+//     return hasil;
+// }
+// var coba = tambah(1,2,3,4,5);
+// console.log(coba);
+
+    // arguments.length //.length berpungsi untuk menghitung index yang ada pada arguments karna
+    // arguments berbentuk array
+    // itulah penyebabnya arguments bisa menerima argumen yang di kirim.
